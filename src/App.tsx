3c8703b11c2a51243f55cdf0f0ff8fd5f69a6c12
Anubhav Tripathi <anubhavtripathinmt_aiml23@its.edu.in>
2025-08-21 20:26:23 +0530

    fix: set basename for BrowserRouter to support routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="/Algoplay">
     <Toaster />
     <Sonner />
     <Routes>
